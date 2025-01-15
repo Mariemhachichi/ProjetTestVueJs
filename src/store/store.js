a2b57fbd612a2f73ab52ac0be44a9299b01c6925
Mariemhachichi <hchichimariem@hotmail.fr>
2025-01-15 17:01:44 +0100

    state et vuex

--- src/store/store.js
+++ src/store/store.js
@@ -4,7 +4,14 @@ import { createStore } from 'vuex';
 const store = createStore({
   state() {
     return {
-      count: 0
+      count: 0,
+      nom: 'ahmed',
+      Etudiants : [
+        {name:'sami',age:20},
+        {name:'rami',age:30},
+        {name:'mounir',age:36},
+        {name:'samia',age:45}
+      ]
     };
   }
 });
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,6 +2,19 @@
   <div>
     <h1>This is an about page</h1>
     <h1>{{ calc }}</h1>
+    <h1>{{ Nom }}</h1>
+    <button v-on:click="Decrmnt">--</button>
+    <button v-on:click="Increment">++</button>
+    <table border="2">
+      <tr>
+        <td>Nom</td>
+        <td>Age</td>
+      </tr>
+      <tr v-for="i in list" :key='i.age'>
+        <td>{{ i.name }}</td>
+        <td>{{ i.age }}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -10,8 +23,22 @@ export default {
   name: 'AboutView',
   computed: {
     calc() {
-    
       return this.$store.state.count;
+    },
+    Nom(){
+      return this.$store.state.nom
+    },
+    list(){
+        return this.$store.state.Etudiants
+    }
+  },
+  methods: {
+    Decrmnt(){
+      return this.$store.state.count--
+    },
+    Increment(){
+      return this.$store.state.count++
+
     }
   }
 }
